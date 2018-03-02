bf5bd6e582568cf5951f96b4fc3ee9893358422f
Dean Meucci <deanmeucci_2018@depauw.edu>
2018-03-02 20:09:54 +0000

    Basic Pages setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+  #homepage
+  root'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
